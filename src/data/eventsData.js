26d5adf0a0c6a644422c4088a4da22366692203e
Eeli <eeli.klemettila@gmail.com>
2022-11-16 23:25:26 +0200

    StylingV2

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -4,7 +4,18 @@ function createData(id, event, time, customer) {
 }
 
 export const eventsReady = [
-  createData(0, "Hiekoituslaatikko täytetty", "14:55", "As Oy Bulevardi"),
-  createData(1, "Hiekoitus toimitettu", "14:45", "As Oy Bulevardi"),
-  createData(2, "Hiekoitus toimitettu", "11:25", "As Oy Aapola"),
+  createData(
+    0,
+    "Hiekoituslaatikko täytetty",
+    "01.01.2022 klo 14:55",
+    "As Oy Bulevardi"
+  ),
+  createData(
+    1,
+    "Hiekoitus toimitettu",
+    "02.01.2022 klo 14:45",
+    "As Oy Bulevardi"
+  ),
+  createData(2, "Hiekoitus toimitettu", "03.01.2022 klo 11:25", "As Oy Aapola"),
+  createData(3, "Hiekoitus toimitettu", "04.01.2022 klo 11:25", "As Oy Aapola"),
 ];
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function Settings() {
   const Item = styled(Paper)(({ theme }) => ({
@@ -11,42 +12,48 @@ export default function Settings() {
     ...theme.typography.body2,
     borderRadius: 15,
     textAlign: "left",
+    margin: 10,
+    padding: 10,
     color: theme.palette.text.secondary,
   }));
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ height: "92vh" }}>
       <Grid container spacing={2}>
         <Grid item xs={12} lg={12}>
-          <Grid>Asetukset</Grid>
-        </Grid>
-        <Grid item xs={6} lg={6}>
-          <Item>
-            <Grid>
-              <Button>Muokkaa profiilia</Button>
-            </Grid>
-            <Grid>
-              <Button>Vaihda salasana</Button>
-            </Grid>
-            <Grid>
-              <Button>Tumma tila</Button>
-            </Grid>
-            <Grid>
-              <Button>Ilmoitukset</Button>
-            </Grid>
-          </Item>
-
-          <Item>
-            <Grid>
-              <Button>Tietoa meistä</Button>
-            </Grid>
-            <Grid>
-              <Button>Tieto- ja yksityisyyssuoja</Button>
-            </Grid>
-          </Item>
-        </Grid>
-        <Grid item xs={6} lg={6}>
-          <Item>{/*Settings display*/}</Item>
+          <Grid item xs={6} lg={6}>
+            <Item>
+              <Typography variant="h6" gutterBottom>
+                Asetukset
+              </Typography>
+            </Item>
+          </Grid>
+          <Grid item xs={6} lg={6}>
+            <Item>
+              <Grid>
+                <Button>Muokkaa profiilia</Button>
+              </Grid>
+              <Grid>
+                <Button>Vaihda salasana</Button>
+              </Grid>
+              <Grid>
+                <Button>Tumma tila</Button>
+              </Grid>
+              <Grid>
+                <Button>Ilmoitukset</Button>
+              </Grid>
+            </Item>
+          </Grid>
+          <Grid item xs={6} lg={6}>
+            <Item>
+              <Grid>
+                <Button>Tietoa meistä</Button>
+              </Grid>
+              <Grid>
+                <Button>Tieto- ja yksityisyyssuoja</Button>
+              </Grid>
+            </Item>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
